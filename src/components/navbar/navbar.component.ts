d12dfefbf09662be25ed508172f197937bb4f5f9
DanielKriegerS <danielschifelbain@gmail.com>
2024-12-09 18:06:18 -0300

    Add function of login button + show login and close nav funcs

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { NavigationService } from '../../app/services/navigation.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(private navigationService: NavigationService, private el: ElementRef) {}
 
+  showComponent(componentName: string): void {
+    this.navigationService.setActiveComponent(componentName);
+    this.closeNavbar(); 
+  }
+
+  closeNavbar(): void {
+    const navbar = this.el.nativeElement.querySelector('#navbarContent');
+    if (navbar && navbar.classList.contains('show')) {
+      navbar.classList.remove('show');
+    }
+  }
 }
