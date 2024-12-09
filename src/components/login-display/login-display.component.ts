ba9f9eb6afa6d5dac73d53d5267f44a096cfb8b0
DanielKriegerS <danielschifelbain@gmail.com>
2024-12-09 17:56:14 -0300

    Created login display component

---
+++ src/components/login-display/login-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-display.component.html',
+  styleUrl: './login-display.component.scss'
+})
+export class LoginDisplayComponent {
+
+}
