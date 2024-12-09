12d6ccfe297c52e290175f96c3d7bc9f22f7a1d2
DanielKriegerS <danielschifelbain@gmail.com>
2024-12-09 18:06:50 -0300

    Add login display component to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../components/header/header.component';
-
+import { NavigationService } from './services/navigation.service';
+import { HeaderComponent } from "../components/header/header.component";
+import { LoginDisplayComponent } from "../components/login-display/login-display.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [HeaderComponent, LoginDisplayComponent, CommonModule],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'headspace';
+  activeComponent: string | null = null;
+
+  constructor(private navigationService: NavigationService) {
+    this.navigationService.activeComponent$.subscribe((componentName) => {
+      this.activeComponent = componentName;
+    });
+  }
 }
