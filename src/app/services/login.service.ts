3ede8433ccea4019e4d871c74fe8d062ddd091ee
DanielKriegerS <danielschifelbain@gmail.com>
2024-12-18 12:09:50 -0300

    Created login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { LoginResponse } from '../../models/LoginResponse';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/users/login';
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, { email, password }).pipe(
+      tap((response: LoginResponse) => {
+        localStorage.setItem('authToken', response.token); 
+      })
+    );
+  }
+  
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
 }
+
