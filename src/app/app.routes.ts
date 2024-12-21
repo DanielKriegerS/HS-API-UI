5bebf219b17407d9e5baea327e4db7ffe6a75082
DanielKriegerS <danielschifelbain@gmail.com>
2024-12-21 18:30:35 -0300

    Feat: register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginDisplayComponent } from '../components/login-display/login-display.component';
+import { RegisterDisplayComponent } from '../components/register-display/register-display.component';
 
 
 export const appRoutes: Routes = [
   { path: 'login', component: LoginDisplayComponent }, 
+  { path: 'register', component: RegisterDisplayComponent},
   { path: '**', redirectTo: '' }, 
 ];
