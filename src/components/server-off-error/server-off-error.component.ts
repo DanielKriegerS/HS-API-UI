cfd3062130ab1e7fc69571222116f87ceaff551b
dschifel <danielschifelbain@gmail.com>
2025-01-02 18:43:20 -0300

    Feat: created refresh timer to check server status

--- src/components/server-off-error/server-off-error.component.ts
+++ src/components/server-off-error/server-off-error.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, switchMap, takeWhile } from 'rxjs';
+import { ServerStatusService } from '../../app/services/server-status.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-off-error',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
   templateUrl: './server-off-error.component.html',
   styleUrl: './server-off-error.component.scss'
 })
-export class ServerOffErrorComponent {
+export class ServerOffErrorComponent implements OnInit, OnDestroy {
+  private subscription!: Subscription;
+  public message: string = 'Tentando reconectar ao servidor...';
 
+  constructor(
+    private serverStatusService: ServerStatusService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = interval(5000)
+      .pipe(
+        switchMap(() => this.serverStatusService.checkServerStatus()),
+        takeWhile((isOnline) => !isOnline, true)
+      )
+      .subscribe({
+        next: (isOnline) => {
+          if (isOnline) {
+            this.message = 'Servidor reconectado! Redirecionando...';
+            setTimeout(() => this.router.navigate(['']), 2000); 
+          }
+        },
+        error: (err) => {
+          console.error('Erro ao verificar status do servidor:', err);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
